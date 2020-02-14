2c46077aab0c3a1dec45c10b37b6eade0e402c35
Bruja <sebaspe12345@gmail.com>
2020-02-13 23:24:48 -0500

    hoy

--- config_connection/connection.js
+++ config_connection/connection.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-var connection = mongoose.connect('mongodb+srv://Bruja:monstermash@cluster0-hp4pe.mongodb.net/Prueba?retryWrites=true&w=majority');
+var connection = mongoose.connect('mongodb+srv://Bruja:1925BscJp@cluster0-hp4pe.mongodb.net/Prueba?retryWrites=true&w=majority');
 
 mongoose.connection.on('open',(ref)=>{
     console.log('Conectado a mongodb')
